5068dcde14eea01fb8094ceb937ed39cd842286b
Nabeelcodes110 <parvenabeel@gmail.com>
2023-03-18 14:30:41 +0530

    Idea selected

---
+++ client/src/pages/Login/Login.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { Col, Button, Row, Container, Card, Form } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+// import './Login.css'
+
+function Login() {
+    return (
+        <div>
+            <Container>
+                <Row className="vh-100 d-flex justify-content-center align-items-center">
+                    <Col md={8} lg={6} xs={12}>
+                        <Card className="px-4">
+                            <Card.Body>
+                                <div className="mb-3 mt-md-4">
+                                    <h2 className="fw-bold mb-2 text-center text-uppercase ">
+                                        Logo
+                                    </h2>
+                                    <div className="mb-3">
+                                        <Form>
+                                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                                <Form.Label className="text-center">
+                                                    Email address
+                                                </Form.Label>
+                                                <Form.Control type="email" placeholder="Enter email" />
+                                            </Form.Group>
+
+                                            <Form.Group
+                                                className="mb-3"
+                                                controlId="formBasicPassword"
+                                            >
+                                                <Form.Label>Password</Form.Label>
+                                                <Form.Control type="password" placeholder="Password" />
+                                            </Form.Group>
+                                            <Form.Group
+                                                className="mb-3"
+                                                controlId="formBasicCheckbox"
+                                            ></Form.Group>
+                                            <div className="d-grid">
+                                                <Button variant="primary" type="submit">
+                                                    Sign in
+                                                </Button>
+                                            </div>
+                                        </Form>
+                                        <div className="mt-3">
+                                            <p className="mb-0  text-center">
+                                                Dont have an account??{' '}
+                                                <Link to="/signup" className="text-primary fw-bold">
+                                                    Sign Up
+                                                </Link>
+                                            </p>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Login
