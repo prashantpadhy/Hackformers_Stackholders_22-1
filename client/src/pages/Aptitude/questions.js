1d58e565d2b76a418a6c1306ea85206919ed5b6c
Prathamrangras <prathament17@gmail.com>
2023-03-18 23:08:07 +0530

    questions added

--- client/src/pages/Aptitude/questions.js
+++ client/src/pages/Aptitude/questions.js
@@ -1,4 +1,4 @@
-export var quiz = {
+export var basic = {
     topic: 'Javascript',
     level: 'Beginner',
     totalQuestions: 4,
@@ -6,48 +6,222 @@ export var quiz = {
     questions: [
       {
         question:
-          'Which function is used to serialize an object into a JSON string in Javascript?',
-        choices: ['stringify()', 'parse()', 'convert()', 'None of the above'],
+          'Do you find websites fascinating?',
+        choices: ['NO' , 'MAYBE' ,'YES'],
+        type: 'Web',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Have you ever wanted to build a website or web Application?',
+        choices: ['NO' , 'MAYBE' ,'YES'],
+        type: 'Web',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Have you heard of cryptocurrencies like Bitcoin,Ethereum or Dodgecoin?',
+        choices: ['NO' , 'YES'],
+        type: 'BC',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Do you know the concept of NFT(Non-fungible tokens?',
+        choices: ['NO' , 'YES'],
+        type: 'BC',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Are you interested in learning how to use data to make predictions or decisions?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'ML',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Do you have an interest in using machine learning to solve real-world problems such as predicting stock prices or detecting frauds?',
+        choices: ['NO' , 'YES'],
+        type: 'ML',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Are you interested in working with connected devices and sensors?',
+        choices: ['NO' , 'MAYBE' ,'yes'],
+        type: 'IOT',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Are you interested in learning about how IOT can be used in applications such as smart cities and autonomous vehicles?',
+        choices: ['NO' , 'MAYBE' ,'YES'],
+        type: 'IOT',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Have you heard about cloud computing before?',
+        choices: ['NO' , 'YES'],
+        type: 'CC',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          'Do you have an interest in learning about how businesses and organizations use cloud computing to store and manage their data?',
+        choices: ['NO' , 'MAYBE' ,'YES'],
+        type: 'CC',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
         type: 'MCQs',
         correctAnswer: 'stringify()',
       },
       {
         question:
-          'Which of the following keywords is used to define a variable in Javascript?',
-        choices: ['var', 'let', 'var and let', 'None of the above'],
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
         type: 'MCQs',
-        correctAnswer: 'var and let',
+        correctAnswer: 'stringify()',
       },
       {
         question:
-          'Which of the following methods can be used to display data in some form using Javascript?',
-        choices: [
-          'document.write()',
-          'console.log()',
-          'window.alert',
-          'All of the above',
-        ],
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
         type: 'MCQs',
-        correctAnswer: 'All of the above',
+        correctAnswer: 'stringify()',
       },
       {
-        question: 'How can a datatype be declared to be a constant type?',
-        choices: ['const', 'var', 'let', 'constant'],
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
         type: 'MCQs',
-        correctAnswer: 'const',
+        correctAnswer: 'stringify()',
       },
       {
-        question: 'Which of the following is a Javascript data type?',
-        choices: ['number', 'function', 'array', 'All of the above'],
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
         type: 'MCQs',
-        correctAnswer: 'All of the above',
+        correctAnswer: 'stringify()',
       },
       {
-        question: 'Which of the following is a Javascript framework?',
-        choices: ['ReactJS', 'AngularJS', 'NodeJs', 'All of the above'],
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
         type: 'MCQs',
-        correctAnswer: 'All of the above',
+        correctAnswer: 'stringify()',
       },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      {
+        question:
+          '?',
+        choices: ['no' , 'maybe' ,'yes'],
+        type: 'MCQs',
+        correctAnswer: 'stringify()',
+      },
+      
       
   
     ],
