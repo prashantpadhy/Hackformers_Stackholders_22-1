961d9dd2b8b37d60d28983bd40e11d71ea27a881
Nabeelcodes110 <parvenabeel@gmail.com>
2023-03-18 14:18:38 +0530

    Idea selected

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-import Login from './pages/Login/Login';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route
+// } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+   <div>
+    Hii
+   </div>
+
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Signup />
+    <App />
   </React.StrictMode>
 );
 
